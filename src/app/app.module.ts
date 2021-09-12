68a0254db7b8adc626154277e44478d6cdd38afa
imarl02116 <60187452+imarl02116@users.noreply.github.com>
2021-09-13 03:02:40 +0900

    productモジュールの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ProductListComponent,
-    ProductDetailComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
