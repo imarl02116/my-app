0ede872cecac5919a65de21350bdc907688db731
imarl02116 <60187452+imarl02116@users.noreply.github.com>
2023-07-23 15:07:40 +0900

    productモジュールの作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductModule } from './product/product.module';
 
-const routes: Routes = [{ path: '', component: ProductListComponent },
-{ path: 'detail', component: ProductDetailComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  // { path: 'detail', component: ProductDetailComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProductModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
