96521174ef0aea9f2c06e4d58fdcd6d864629490
imarl02116 <60187452+imarl02116@users.noreply.github.com>
2023-07-23 17:18:13 +0900

    navbar階層変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './common/navbar/navbar/navbar.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
