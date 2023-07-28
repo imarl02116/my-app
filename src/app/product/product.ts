4cfeb61a4a2990c7ae53965d1c7612ec4a5ff68d
imarl02116 <60187452+imarl02116@users.noreply.github.com>
2023-07-28 17:47:12 +0900

    商品のテキスト部分を動的に対応させよう

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -3,6 +3,12 @@ export interface Product {
     name: string;
     price: number;
     description: string;
+    heading1: string;
+    heading2: string;
+    heading3: string;
+    headingtext1: string;
+    headingtext2: string;
+    headingtext3: string;
 }
 
 export const products = [
@@ -10,18 +16,36 @@ export const products = [
         id: 0,
         name: 'Phone XL',
         price: 799,
-        description: 'A large phone with one of the best screens'
+        description: 'A large phone with one of the best screens',
+        heading1: 'Heading1',
+        heading2: 'Heading2',
+        heading3: 'Heading3',
+        headingtext1: 'X sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+        headingtext2: 'X mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'X sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
     },
     {
         id: 1,
         name: 'Phone Mini',
         price: 699,
-        description: 'A great phone with one of the best cameras'
+        description: 'A great phone with one of the best cameras',
+        heading1: 'Heading1',
+        heading2: 'Heading2',
+        heading3: 'Heading3',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
     },
     {
         id: 2,
         name: 'Phone Standard',
         price: 299,
-        description: ''
+        description: '',
+        heading1: 'Heading1',
+        heading2: 'Heading2',
+        heading3: 'Heading3',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
     }
 ];
