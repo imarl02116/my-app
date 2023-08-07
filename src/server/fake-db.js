efb366b77bd22d0016f1d26c69ad8486333a85b5
imarl02116 <60187452+imarl02116@users.noreply.github.com>
2023-08-07 15:55:46 +0900

    MongoDBにサンプルデータを保存

---
+++ src/server/fake-db.js
@@ -0,0 +1,69 @@
+const Product = require('./model/product')
+
+class FakeDB {
+
+    constructor() {
+        this.products = [
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                heading1: 'Heading1',
+                heading2: 'Heading2',
+                heading3: 'Heading3',
+                headingtext1: 'X sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+                headingtext2: 'X mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingtext3: 'X sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
+            },
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                heading1: 'Heading1',
+                heading2: 'Heading2',
+                heading3: 'Heading3',
+                headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+                headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
+            },
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Standard',
+                price: 299,
+                description: '',
+                heading1: 'Heading1',
+                heading2: 'Heading2',
+                heading3: 'Heading3',
+                headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.  ',
+                headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus'
+            }
+        ]
+    }
+
+    async initDb() {
+        await this.cleanDb()
+        this.pushProductsToDb()
+    }
+
+    async cleanDb() {
+        await Product.deleteMany({})
+    }
+
+    pushProductsToDb() {
+        this.products.forEach(
+            (product) => {
+                const newProduct = new Product(product)
+                newProduct.save()
+            }
+        )
+    }
+
+    seeDb() {
+        this.pushProductsToDb()
+    }
+}
+
+module.exports = FakeDB
---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const config = require('./config/dev')
+const FakeDb = require('./fake-db')
+const products = require('./routes/products')
+
+mongoose.connect(config.DB_URI).then(
+    () => {
+        const fakeDb = new FakeDb()
+        fakeDb.initDb()
+    }
+);
+
+const app = express()
+
+const PORT = process.env.PORT | '3001'
+
+app.use('/products', products)
+
+app.listen(PORT, function () {
+    console.log('I am running!')
+})
